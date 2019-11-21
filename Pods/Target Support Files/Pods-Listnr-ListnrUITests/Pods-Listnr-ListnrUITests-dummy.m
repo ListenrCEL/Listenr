8bb37051cc2db9c9102f1b1b06977776b4c91170
OliverMoscow <OliverMoscow@users.noreply.github.com>
2019-11-21 13:00:03 -0500

    10/21/19 podfiles

---
+++ Pods/Target Support Files/Pods-Listnr-ListnrUITests/Pods-Listnr-ListnrUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Listnr_ListnrUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Listnr_ListnrUITests
+@end
---
+++ Pods/Target Support Files/Pods-Listnr/Pods-Listnr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Listnr : NSObject
+@end
+@implementation PodsDummy_Pods_Listnr
+@end
